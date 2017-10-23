73adf932da0bd1e8a98784e71ce42b737316e115
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-10-23 18:43:04 +0200

    Initial version

---
+++ resources/assets/js/components/MessageComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="alert alert-danger alert-dismissible" v-if="visible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-ban"></i> {{ dataTitle }}</h4>
+        {{ dataMessage }}
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+    export default {
+      data() {
+        return {
+          visible: true,
+          dataMessage: this.message,
+          dataTitle: this.title,
+        }
+      },
+      props: {
+        'title': {
+          required: false
+        },
+        'message': {
+          required: true
+        }
+      },
+      mounted() {
+        console.log('Mounted!')
+        console.log(this.message)
+      }
+    }
+</script>
---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    Tasks
+@endsection
+
+@section('main-content')
+    <tasks></tasks>
+
+    <message title="Error"></message>
+
+    <message title="Error" message="Error greu"></message>
+
+    <message message="Error greu"></message>
+
+    <message></message>
+
+    {{--<message title="Error">Error ha petat tot!</message>--}}
+
+
+    {{--<message title="Error" message="Error ha petat tot!"></message>--}}
+
+    {{--<message>--}}
+        {{--<slot name="title">Error</slot>--}}
+        {{--<slot name="message">Error ha petat tot</slot>--}}
+    {{--</message>--}}
+@endsection
