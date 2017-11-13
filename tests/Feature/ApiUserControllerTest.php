96aaa382d6be1c8e0fbd698480ad25f37831c4d8
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-11-13 22:00:28 +0100

    Sync

--- tests/Feature/ApiUserControllerTest.php
+++ tests/Feature/ApiUserControllerTest.php
@@ -22,7 +22,7 @@ class ApiUserControllerTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->withoutExceptionHandling();
+//        $this->withoutExceptionHandling();
     }
 
     /**
@@ -50,17 +50,17 @@ class ApiUserControllerTest extends TestCase
     }
 
     /**
-     * Can show a task.
+     * Can show a user.
      * @test
      */
-    public function can_show_a_task()
+    public function can_show_a_user()
     {
         $user = factory(User::class)->create();
 
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
+        $loggedUser = factory(User::class)->create();
+        $this->actingAs($loggedUser);
 
-        $response = $this->json('GET', '/api/users/' . $user->id);
+        $response = $this->json('GET', '/api/v1/users/' . $user->id);
 
         $response->assertSuccessful();
 
@@ -73,12 +73,12 @@ class ApiUserControllerTest extends TestCase
     /**
      * @test
      */
-    public function cannot_add_task_if_not_logged()
+    public function cannot_add_user_if_not_logged()
     {
         $faker = Factory::create();
 
         // EXECUTE
-        $response = $this->json('POST', '/api/users', [
+        $response = $this->json('POST', '/api/v1/users', [
             'name' => $name = $faker->word
         ]);
 
@@ -89,13 +89,13 @@ class ApiUserControllerTest extends TestCase
     /**
      * @test
      */
-    public function cannot_add_task_if_no_name_provided()
+    public function cannot_add_user_if_no_name_provided()
     {
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
+        $loggedUser = factory(User::class)->create();
+        $this->actingAs($loggedUser);
 
         // EXECUTE
-        $response = $this->json('POST', '/api/users');
+        $response = $this->json('POST', '/api/v1/users');
 
         // ASSERT
         $response->assertStatus(422);
@@ -104,7 +104,7 @@ class ApiUserControllerTest extends TestCase
     /**
      * @test
      */
-    public function can_add_a_task()
+    public function can_add_a_user()
     {
         // PREPARE
         $faker = Factory::create();
@@ -113,7 +113,7 @@ class ApiUserControllerTest extends TestCase
         $this->actingAs($user);
 
         // EXECUTE
-        $response = $this->json('POST', '/api/users', [
+        $response = $this->json('POST', '/api/v1/users', [
             'name' => $name = $faker->word
         ]);
 
@@ -134,14 +134,14 @@ class ApiUserControllerTest extends TestCase
     /**
      * @test
      */
-    public function can_delete_task()
+    public function can_delete_user()
     {
         $user = factory(User::class)->create();
         $user = factory(User::class)->create();
 
         $this->actingAs($user);
 
-        $response = $this->json('DELETE','/api/users/' . $user->id);
+        $response = $this->json('DELETE','/api/v1/users/' . $user->id);
 
         $response->assertSuccessful();
 
@@ -158,13 +158,13 @@ class ApiUserControllerTest extends TestCase
     /**
      * @test
      */
-    public function cannot_delete_unexisting_task()
+    public function cannot_delete_unexisting_user()
     {
         $user = factory(User::class)->create();
 
         $this->actingAs($user);
 
-        $response = $this->json('DELETE','/api/users/1');
+        $response = $this->json('DELETE','/api/v1/users/1');
 
         $response->assertStatus(404);
     }
@@ -172,16 +172,16 @@ class ApiUserControllerTest extends TestCase
     /**
      * @test
      */
-    public function can_edit_task()
+    public function can_edit_user()
     {
         // PREPARE
-        $user = factory(User::class)->create();
+        $user1 = factory(User::class)->create();
 
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
         // EXECUTE
-        $response = $this->json('PUT', '/api/users/' . $user->id, [
+        $response = $this->json('PUT', '/api/v1/users/' . $user1->id, [
             'name' => $newName = 'NOU NOM'
         ]);
 
